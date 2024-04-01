46f4daa2ec76933b79be8ad8dd1c02452b9ad3a4
nicolas <nicolas.mine11@gmail.com>
2024-04-01 18:41:57 +0200

    with vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,34 @@
 <template>
-  <div class="container flex items-center justify-center m-auto mt-12">
-    <div>
-      <header>
-        <Logo />
-        <h1 class="text-2xl text-gray-900 font-extrabold mt-2">
-          nuxt-hexagonal-architecture-playground
-        </h1>
-      </header>
-      <main
-        class="mt-10 bg-green-100 p-10 border-2 border-solid border-indigo-200"
-      >
-        <List></List>
-        <Create class="mt-5"></Create>
-      </main>
-      <footer></footer>
+  <v-app>
+    <div class="container flex items-center justify-center m-auto mt-12">
+      <div>
+        <header>
+          <Logo/>
+          <h1 class="text-2xl text-gray-900 font-extrabold mt-2">
+            nuxt-hexagonal-architecture-playground
+          </h1>
+        </header>
+        <main
+          class="mt-10 bg-green-100 p-10 border-2 border-solid border-indigo-200"
+        >
+          <List></List>
+          <Create class="mt-5"></Create>
+        </main>
+        <footer></footer>
+      </div>
     </div>
-  </div>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component } from 'nuxt-property-decorator';
+import {Component} from 'nuxt-property-decorator';
 import Vue from 'vue';
 import Create from '@/pages/todo/Create.vue';
 import List from '@/pages/todo/List.vue';
 
 @Component({
-  components: { List, Create },
+  components: {List, Create},
 })
-export default class Index extends Vue {}
+export default class Index extends Vue {
+}
 </script>
--- pages/todo/Create.vue
+++ pages/todo/Create.vue
@@ -8,14 +8,14 @@
         placeholder="task"
         aria-label="taks"
       />
-      <button
+      <v-btn
         class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
         type="button"
         tabindex="-1"
         @click="addTask"
       >
         Create
-      </button>
+      </v-btn>
     </div>
   </form>
 </template>
